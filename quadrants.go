3dc76b9677bed4b3bbeff910d18ededb268cf4a9
beistvan <beistvan@gmail.com>
2025-04-05 12:24:33 +0200

    Create quadrants.go

---
+++ quadrants.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x, y float64
+    fmt.Scan(&x, &y)
+
+    if x > 0 && y > 0 {
+        fmt.Println(1)
+    } else if x < 0 && y > 0 {
+        fmt.Println(2)
+    } else if x < 0 && y < 0 {
+        fmt.Println(3)
+    } else if x > 0 && y < 0 {
+        fmt.Println(4)
+    }
+}
