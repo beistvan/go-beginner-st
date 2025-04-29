03f0a7e6240648aa869550ea8a463b9cd18fc75e
beistvan <beistvan@gmail.com>
2025-04-29 11:04:30 +0200

    reverse-string.go

---
+++ reverse-string.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	input = input[:len(input)-1]
+
+	runes := []rune(input)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	fmt.Println(string(runes))
+}
